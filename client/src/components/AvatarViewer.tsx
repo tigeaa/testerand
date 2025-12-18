fe2378a3075232d76e84577e8679af1bfd329046
tigeaa <toranet.fugenjikko@gmail.com>
2025-12-18 14:27:47 +0900

    Scale avatar model to 3
    Increased the avatar model scale from 1 to 3.

--- client/src/components/AvatarViewer.tsx
+++ client/src/components/AvatarViewer.tsx
@@ -96,7 +96,7 @@ export default function AvatarViewer({ avatarUrl, animationUrls }: AvatarViewerP
         avatarUrl,
         (gltf: any) => {
           const model = gltf.scene;
-          model.scale.set(1, 1, 1);
+          model.scale.set(3, 3, 3);
           // Adjust model position to center it
           model.position.y = -1.0;
           scene.add(model);
