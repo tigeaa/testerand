f6f1c411423612985c263aae3f488070ddd446b2
tigeaa <toranet.fugenjikko@gmail.com>
2025-12-18 14:45:55 +0900

    Reduce avatar model scale from 10 to 5

--- client/src/components/AvatarViewer.tsx
+++ client/src/components/AvatarViewer.tsx
@@ -96,7 +96,7 @@ export default function AvatarViewer({ avatarUrl, animationUrls }: AvatarViewerP
         avatarUrl,
         (gltf: any) => {
           const model = gltf.scene;
-          model.scale.set(10, 10, 10);
+          model.scale.set(5, 5, 5);
           // Adjust model position to center it
           model.position.y = -1.0;
           scene.add(model);
